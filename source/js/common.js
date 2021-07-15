f0841dec2284ad13f818877c2977d68bf003dde2
Andrey <soinproduction@gmail.com>
2021-07-15 18:16:19 +0300

    create second sec

--- source/js/common.js
+++ source/js/common.js
@@ -1,56 +1,73 @@
-const imageBox = document.getElementById("images");
+// const imageBox = document.getElementById("images");
 
-const images = imageBox.querySelectorAll(".round-wrapp");
+// const images = imageBox.querySelectorAll(".round-wrapp");
 
-let delay = 350;
+// let delay = 350;
 
-[...images].map((img) => {
-  setTimeout(() => {
-    img.classList.add("round-wrapp--visible");
-  }, delay);
-  delay += 350;
-});
+// [...images].map((img) => {
+//   setTimeout(() => {
+//     img.classList.add("round-wrapp--visible");
+//   }, delay);
+//   delay += 350;
+// });
 
-(document.getElementById("burger-btn").onclick = function () {
-  this.classList.toggle("burger-btn--active"),
-    document
-    .getElementById("header-menu")
-    .classList.toggle("header-menu--active");
-}),
-(document.getElementById("close").onclick = function () {
-  document
-    .getElementById("burger-btn")
-    .classList.remove("burger-btn--active"),
-    document
-    .getElementById("header-menu")
-    .classList.remove("header-menu--active");
-});
+// (document.getElementById("burger-btn").onclick = function () {
+//   this.classList.toggle("burger-btn--active"),
+//     document
+//     .getElementById("header-menu")
+//     .classList.toggle("header-menu--active");
+// }),
+// (document.getElementById("close").onclick = function () {
+//   document
+//     .getElementById("burger-btn")
+//     .classList.remove("burger-btn--active"),
+//     document
+//     .getElementById("header-menu")
+//     .classList.remove("header-menu--active");
+// });
 
 
-AOS.init();
+// AOS.init();
 
-document.addEventListener("DOMContentLoaded", function () {
-  for (var e = document.links, t = 0, n = e.length; t < n; t++) o(e[t]);
+// document.addEventListener("DOMContentLoaded", function () {
+//   for (var e = document.links, t = 0, n = e.length; t < n; t++) o(e[t]);
 
-  function o(e) {
-    if (e.hostname === location.hostname && e.pathname === location.pathname) {
-      var t = e.hash.slice(1) ?
-        document.getElementById(e.hash.slice(1)) :
-        document.body;
-      t &&
-        e.addEventListener("click", function (e) {
-          e.preventDefault(),
-            (function (e) {
-              var t = 0,
-                n = e / 90,
-                o = window.pageYOffset || document.documentElement.scrollTop,
-                c = setInterval(function () {
-                  (o += n),
-                  window.scrollTo(0, o),
-                    ++t >= 90 && clearInterval(c);
-                }, 1);
-            })(t.getBoundingClientRect().top);
-        });
-    }
-  }
-});
+//   function o(e) {
+//     if (e.hostname === location.hostname && e.pathname === location.pathname) {
+//       var t = e.hash.slice(1) ?
+//         document.getElementById(e.hash.slice(1)) :
+//         document.body;
+//       t &&
+//         e.addEventListener("click", function (e) {
+//           e.preventDefault(),
+//             (function (e) {
+//               var t = 0,
+//                 n = e / 90,
+//                 o = window.pageYOffset || document.documentElement.scrollTop,
+//                 c = setInterval(function () {
+//                   (o += n),
+//                   window.scrollTo(0, o),
+//                     ++t >= 90 && clearInterval(c);
+//                 }, 1);
+//             })(t.getBoundingClientRect().top);
+//         });
+//     }
+//   }
+// });
+
+
+var swiper = new Swiper(".mySwiper");
+  // slidesPerView: 3,
+  // spaceBetween: 30,
+  // slidesPerGroup: 3,
+  // loop: true,
+  // loopFillGroupWithBlank: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+
